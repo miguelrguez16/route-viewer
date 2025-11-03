6fc44d1a8be58469ce58b515aa8a0c637554643f
Miguel Rodríguez González <uo263494@uniovi.es>
2025-11-03 22:34:40 +0100

    feat: new route Allaer pr as 265

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Viewer } from './Viewer'
 import { FilterProvider } from './context/FilterContext'
-import Filter from './components/filter/Filter'
-
+ 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div style={{  height: '100vdh' }}>
